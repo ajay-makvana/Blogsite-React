d2cf052a1dda3576ebc52f918ccdd387c31d1da5
ajay-makvana <ajaymakvana2001@gmail.com>
2023-11-05 21:28:46 +0530

    header, footer created and components/index.js changed

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,31 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import PostForm from "./PostForm/PostForm";
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import Input from "./Input"
+import Login from "./Login";
+import Logo from "./Logo";
+import PostCard from "./PostCard"
+import RTE from "./RTE";
+import LogoutBtn from "./Header/LogoutBtn";
+import SelectBtn from "./SelectBtn"
+import Signup from "./SignUp";
 
-export { Header, Footer };
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Input,
+    Button,
+    PostCard,
+    SelectBtn,
+    RTE,
+    Login,
+    Signup,
+    PostForm,
+    AuthLayout,
+};
